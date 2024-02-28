ed3f8df6e2c3c1a2413ce0f917b75b1af4fea498
LEARN Academy <learnacademy@macbook-air-2.lan>
2024-02-28 13:29:28 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import "./App.css"
-
+import Square from "./components/Square"
 const App = () => {
   const [board, setBoard] = useState([
     "?",
@@ -13,12 +13,35 @@ const App = () => {
     "?",
     "?"
   ])
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length)) 
+
+  const handleGameplay = (currentSquare)=>{
+    if (currentSquare === treasureLocation) { 
+      board[currentSquare] = "💎";
+ }else {
+    board[currentSquare] = "🌴"
+  }
+     setBoard([...board])
+}
+
+
 
   return (
     <>
       <h1>Treasure Hunt Game</h1>
+      <div className="board">
+        {board.map((square, index)=> (
+         <Square
+          key={index} 
+          square= {square} 
+          index={index} 
+          handleGameplay={handleGameplay}/>
+        ))}
+      </div>
     </>
-  )
+  );
 }
+        
+
 
 export default App
